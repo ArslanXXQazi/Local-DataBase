b73320da5080c2521f8ec287b58aacbfafb23b81
Arslan Qazi <arslanqazi1999@gmail.com>
2025-01-27 10:07:14 +0500

    Update insert_view.dart

---
+++ lib/view/home_views/insert_view/insert_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class InsertView extends StatelessWidget {
+  const InsertView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+            'Insert Data',
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+      ),
+       body: Column(children: [
+
+       ],),
+    );
+  }
+}
