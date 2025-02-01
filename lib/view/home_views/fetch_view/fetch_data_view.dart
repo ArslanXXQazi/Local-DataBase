44801f72a608c5f548940015108dc7c1056be475
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-01 15:03:50 +0500

    update

--- lib/view/home_views/fetch_view/fetch_data_view.dart
+++ lib/view/home_views/fetch_view/fetch_data_view.dart
@@ -96,6 +96,7 @@ class _FetchDataViewState extends State<FetchDataView> {
                  ),
                  ),
                );
+
             //    ListTile(
             //   onTap: (){
             //     db.deleteData(data[index].id!);
